ca4e442b6d04f16252eeeb45f54f1d47a9a7d084
Swipeys <69121928+swipeys@users.noreply.github.com>
2021-05-14 23:49:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ client.on('ready', () => {
         activity: {
             details: 'Check out the buttons below!',
             buttons: [
-                { label: 'All Nighters', url: 'https://discord.gg/QabpFsxhxm' },
-                { label: 'Trade Mall', url: 'https://discord.gg/bKc8xrmCT6' } 
+                { label: 'Insert Button name here.', url: 'Insert URL here.' },
+                { label: 'Insert Button name here.', url: 'Insert URL here.' } 
             ]
         }
       })
-    console.log('Started!')
+    console.log('Your Discord RPC has been started.')
 })
-client.login({clientId: '804686328997740624'})
+client.login({clientId: 'Insert Client ID here.'})
